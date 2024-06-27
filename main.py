81832ade9156c7d0544e7f01db15b6e1c52b6eb3
Shayne Sivley <71092239+greaselovely@users.noreply.github.com>
2024-06-27 15:34:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,8 @@ import json
 import logging
 import requests
 from pathlib import Path
-from inventory import clear
 from datetime import datetime, timezone
 from logging.handlers import RotatingFileHandler
-import certifi
-
-# Use certifi's CA bundle
-CERT_PATH = certifi.where()
-
-GREEN_CIRCLE = "\U0001F7E2"
-RED_CIRCLE = "\U0001F534"
-
-# print(f"Using CA Bundle at: {CERT_PATH}")
 
 inventory = "inventory.json"
 log_file_name = "airthings.log"
@@ -22,6 +12,7 @@ home = os.path.expanduser('~')
 log_path = os.path.join(home, "airthings")
 log_full_path = os.path.join(log_path, log_file_name)
 os.makedirs(log_path, exist_ok=True)
+now = datetime.now()
 
 airthings_authorisation_url = "https://accounts-api.airthings.com/v1/token"
 token_req_payload = {"grant_type": "client_credentials", "scope": "read:device:current_values"}
@@ -35,69 +26,146 @@ logger.setLevel(logging.INFO)
 logger.addHandler(log_handler)
 
 def send_ntfy_msg(ntfy_topic: str, message: str):
+    """
+    Send a notification message to a specified ntfy.sh topic.
+
+    Args:
+        ntfy_topic (str): The ntfy.sh topic to send the message to.
+        message (str): The message content to be sent.
+
+    Raises:
+        RequestException: If there's an error sending the message.
+    """
     url = f"https://ntfy.sh/{ntfy_topic}"
     headers = {'Content-Type': 'text/plain; charset=utf-8'}
+    
     try:
-        response = requests.post(url, data=message.encode('utf-8'), headers=headers, verify=CERT_PATH)
+        response = requests.post(url, data=message.encode('utf-8'), headers=headers)
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
         logging.error(f"Failed to send message to {ntfy_topic}: {e}")
 
 def airthings_auth():
+    """
+    Authenticate with the Airthings API and obtain an access token.
+
+    Returns:
+        str: The access token if successful, None otherwise.
+
+    Raises:
+        RequestException: If there's an error during the authentication process.
+    """
     try:
-        token_response = requests.post(airthings_authorisation_url, data=token_req_payload, allow_redirects=False, auth=(airthings_client_id, airthings_client_secret), verify=CERT_PATH)
+        token_response = requests.post(airthings_authorisation_url, data=token_req_payload, allow_redirects=False, auth=(airthings_client_id, airthings_client_secret))
         token_response.raise_for_status()
         token = token_response.json().get("access_token")
-        # print("Token retrieved successfully:", token)  # Debugging information
         return token
     except requests.exceptions.RequestException as e:
         logging.error(f"Failed to obtain Airthings access token: {e}")
         return None
 
-def read_inventory(file_name):
+def read_from_file(file_name):
+    """
+    Read and parse JSON data from a file.
+
+    Args:
+        file_name (str): The name of the file to read from.
+
+    Returns:
+        dict: The parsed JSON data.
+    """
     file_path = Path(__file__).resolve().parent / file_name
     with open(file_path, "r") as file:
         return json.load(file)
 
 def convert_timestamp_to_time(timestamp):
+    """
+    Convert a UNIX timestamp to a formatted date-time string.
+
+    Args:
+        timestamp (int): The UNIX timestamp to convert.
+
+    Returns:
+        str: The formatted date-time string.
+    """
     return datetime.fromtimestamp(timestamp, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
 
 def is_data_stale(timestamp: int, freshness_threshold_seconds: int = 3600) -> bool:
+    """
+    Check if the data is stale based on its timestamp.
+
+    Args:
+        timestamp (int): The timestamp of the data.
+        freshness_threshold_seconds (int): The threshold in seconds to consider data stale.
+
+    Returns:
+        bool: True if the data is stale, False otherwise.
+    """
     current_time = datetime.now(timezone.utc).timestamp()
     data_age_seconds = current_time - timestamp
     return data_age_seconds >= freshness_threshold_seconds
 
-def console_output(location, room, c_temp, f_temp, humi, batt, circle):
+def console_output(location, room, c_temp, f_temp, humi, batt):
+    """
+    Print formatted output to the console.
+
+    Args:
+        location (str): The location name.
+        room (str): The room name.
+        c_temp (float): Temperature in Celsius.
+        f_temp (float): Temperature in Fahrenheit.
+        humi (float): Humidity percentage.
+        batt (float): Battery percentage.
+    """
     print(f"\t{location} {room}:")
-    print(f"\t  Online: {circle}")
     print(f"\t  Temp: {f_temp}°F / {c_temp}°C")
     print(f"\t  Humidity: {humi}%")
     print(f"\t  Battery: {batt}%")
 
 def fetch_device_data(device_id, api_headers):
-    device_url = f"https://ext-api.airthings.com/v1/devices/{device_id}/latest-samples"
-    try:
-        response = requests.get(url=device_url, headers=api_headers, verify=CERT_PATH)
-        response.raise_for_status()
-        return response.json()['data']
-    except requests.exceptions.RequestException as e:
-        logging.error(f"Error fetching device data for {device_id}: {e}")
-        return None
+    """
+    Fetch the latest data for a specific device from the Airthings API.
 
-def process_device_data(location, room, device_data, thresholds, ntfy_url, is_sunday):
-    global sunday_report
+    Args:
+        device_id (str): The ID of the device to fetch data for.
+        api_headers (dict): The headers to use for the API request.
+
+    Returns:
+        dict: The latest data for the device.
+
+    Raises:
+        RequestException: If there's an error fetching the device data.
+    """
+    device_url = f"https://ext-api.airthings.com/v1/devices/{device_id}/latest-samples"
+    response = requests.get(url=device_url, headers=api_headers)
+    response.raise_for_status()
+    return response.json()['data']
+
+def process_device_data(location, room, device_data, thresholds, ntfy_url, is_sunday, sunday_report):
+    """
+    Process the data for a single device, checking thresholds and generating reports.
+
+    Args:
+        location (str): The location name.
+        room (str): The room name.
+        device_data (dict): The data for the device.
+        thresholds (dict): The threshold values for various metrics.
+        ntfy_url (str): The ntfy.sh URL for sending notifications.
+        is_sunday (bool): Whether it's Sunday (for weekly reports).
+        sunday_report (str): The ongoing Sunday report string.
+
+    Returns:
+        str: The updated Sunday report string.
+    """
     timestamp, c_temp, humi, batt = device_data['time'], device_data['temp'], device_data['humidity'], device_data['battery']
     
-    stale_message = ""
     if is_data_stale(timestamp, thresholds['freshness_threshold_seconds']):
-        circle = RED_CIRCLE
-        stale_message = f"{location} {room} is stale {circle}."
-    else:
-        circle = GREEN_CIRCLE
+        message = f"Data for {location} {room} is stale."
+        send_ntfy_msg(ntfy_url, message)
     
-    f_temp = (c_temp * 9 / 5) + 32
+    f_temp = (c_temp * 9/5) + 32
     f_temp = float(f"{f_temp:0.2f}")
-    console_output(location, room, c_temp, f_temp, humi, batt, circle)
+    console_output(location, room, c_temp, f_temp, humi, batt)
     logging.info(f"{location} {room} - Temp:{f_temp} Humidity:{humi} Batt:{batt}")
 
     if f_temp <= thresholds['f_temp_threshold']:
@@ -109,19 +177,23 @@ def process_device_data(location, room, device_data, thresholds, ntfy_url, is_su
         send_ntfy_msg(ntfy_url, message)
 
     if is_sunday:
-        sunday_report += f"{location} {room} {circle} @ {f_temp}°F, battery is {batt}%\n"
+        sunday_report += f"{location} {room} is {f_temp}°F, battery is {batt}%\n"
     
-    return stale_message
+    return sunday_report
 
 def main():
-    global airthings_client_id, airthings_client_secret, sunday_report
-    inventory_data = read_inventory('inventory.json')
+    """
+    The main function that orchestrates the Airthings data fetching and processing.
+    """
+    global airthings_client_id, airthings_client_secret
+    inventory_data = read_from_file('inventory.json')
     
     if not inventory_data:
         logging.error("Inventory data is not available. Please check the file.")
         return
-    
-    airthings_client_id, airthings_client_secret = inventory_data["airthings_client_id"], inventory_data["airthings_client_secret"]
+
+    airthings_client_id = inventory_data["airthings_client_id"]
+    airthings_client_secret = inventory_data["airthings_client_secret"]
 
     thresholds = {
         'freshness_threshold_seconds': 3600,
@@ -131,35 +203,24 @@ def main():
 
     token = airthings_auth()
     if not token:
-        logging.error("Failed to retrieve access token, terminating script.")
+        logging.error("Failed to obtain Airthings token. Exiting.")
         return
-    
+
     api_headers = {"Authorization": f"Bearer {token}"}
     now = datetime.now()
     is_sunday = now.weekday() == 6 and now.hour == 17 and now.minute == 0
-    
-    stale_data_messages = []
     sunday_report = "Weekly Report\n"
-
+    
     try:
         for location, rooms in inventory_data["inventory"].items():
-            for room, device_id in rooms.items():
-                device_data = fetch_device_data(device_id, api_headers)
-                
-                if device_data:
-                    stale_message = process_device_data(location, room, device_data, thresholds, inventory_data["ntfy_url"], is_sunday)
-                    if stale_message:
-                        stale_data_messages.append(stale_message)
-                else:
-                    logging.error(f"No data received for device {device_id} in {location} {room}")
+            for room, device_info in rooms.items():
+                if device_info['type'].startswith('WAVE_'):
+                    device_id = device_info['id']
+                    device_data = fetch_device_data(device_id, api_headers)
+                    sunday_report = process_device_data(location, room, device_data, thresholds, inventory_data["ntfy_url"], is_sunday, sunday_report)
         
         if is_sunday:
             send_ntfy_msg(inventory_data["ntfy_url"], sunday_report)
-
-        if stale_data_messages:
-            consolidated_message = "Stale Data Notification:\n" + "\n".join(stale_data_messages)
-            send_ntfy_msg(inventory_data["ntfy_url"], consolidated_message)
-
     except requests.exceptions.RequestException as e:
         logging.error(f"Error fetching device data: {e}")
 
